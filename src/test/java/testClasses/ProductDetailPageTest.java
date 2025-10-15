33a4f359a489ca276f86d0aab47057c5aff10271
Shrikant Barude <shrikantbarude@gmail.com>
2025-10-15 23:35:51 +0530

    feat/ProductDetailPage Added

---
+++ src/test/java/testClasses/ProductDetailPageTest.java
@@ -0,0 +1,41 @@
+package testClasses;
+
+import org.testng.annotations.*;
+
+public class ProductDetailPageTest {
+
+    @BeforeClass
+
+    public void beforeClass()
+    {
+
+    }
+    @BeforeMethod
+
+
+    public void beforeMethod() {
+
+    }
+    @Test
+    public void test() {
+        System.out.println("Test Case Run");
+
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+
+
+    }
+    @AfterClass
+     public void afterClass()
+    {
+
+    }
+
+
+
+
+
+
+}
